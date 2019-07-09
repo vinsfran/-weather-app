fa23f59aa3d43063f23039d983037a3cde422b41
Vicente Insfran <vinsfran@gmail.com>
2019-07-09 17:58:51 -0400

    Clase 21

--- src/components/WeatherData .js
+++ src/components/WeatherData .js
@@ -1,7 +1,12 @@
 import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo ';
+import WeatherTemperature from './WeatherTemperature ';
 
 const WeatherData = () => (
-    <div>Weather Data</div>
+    <div>
+        <WeatherTemperature/>
+        <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
+    </div>
 );
 
 export default WeatherData;
