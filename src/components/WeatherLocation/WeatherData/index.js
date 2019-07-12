983026fa9a9dfa45d000432a854d887a32b9fad8
Vicente Insfran <vinsfran@gmail.com>
2019-07-12 14:41:19 -0400

    Clase 36

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo ';
 import WeatherTemperature from './WeatherTemperature ';
-import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    SNOW,
-    WINDY
-} from '../../../constants/weathers';
 import './styles.css'
 
-const WeatherData = () => (
-    <div className="weatherDataCont" >
+const WeatherData = ({data: {temperature, weatherState, humidity, wind}}) => (
+    <div className="weatherDataCont">
         <WeatherTemperature
-            temperature={20}
-            weatherState={SUN}
+            temperature={temperature}
+            weatherState={weatherState}
         />
-        <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
+        <WeatherExtraInfo humidity={humidity} wind={wind}/>
     </div>
 );
 
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import "./styles.css";
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY
+} from '../../constants/weathers';
+
+const data = {
+    temperature: 6,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s',
+}
 
 const WeatherLocation = () => (
     <div className="weatherLocationCont">
         <Location city={"Barcelona"}></Location>
-        <WeatherData/>
+        <WeatherData data={data}/>
     </div>
 );
 
